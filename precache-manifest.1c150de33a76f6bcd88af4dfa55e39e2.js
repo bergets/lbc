5939215b97b6ad19e633df420a0fd5d661e5f4a2
bergets <henrik.jan.bergstrom@gmail.com>
2019-08-01 11:56:10 +0200

    Test

---
+++ precache-manifest.1c150de33a76f6bcd88af4dfa55e39e2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c7c5bfae97588bc6b3a42d29e2e9231",
+    "url": "/lbc/index.html"
+  },
+  {
+    "revision": "65e782c43a892a84327c",
+    "url": "/lbc/static/css/main.0cce146f.chunk.css"
+  },
+  {
+    "revision": "785b775933eed44dbbaf",
+    "url": "/lbc/static/js/2.b09be9a6.chunk.js"
+  },
+  {
+    "revision": "65e782c43a892a84327c",
+    "url": "/lbc/static/js/main.7b6016ea.chunk.js"
+  },
+  {
+    "revision": "f97c036c54a4f5c20972",
+    "url": "/lbc/static/js/runtime~main.5367beea.js"
+  },
+  {
+    "revision": "6a3df54d2995c73bedeb6660395bc34f",
+    "url": "/lbc/static/media/ch1.6a3df54d.jpg"
+  },
+  {
+    "revision": "79333d473422399666af26a5c74d2222",
+    "url": "/lbc/static/media/ch2.79333d47.jpg"
+  },
+  {
+    "revision": "ee00ef0753a80044a42ac709c9b7cf23",
+    "url": "/lbc/static/media/ch3.ee00ef07.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lbc/static/media/logo.5d5d9eef.svg"
+  }
+]);
